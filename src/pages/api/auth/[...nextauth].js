4c4bd66f8d7d53c23f1a8f127c8669440d4bfc5b
Vikrant Chauhan <vikrantchauhan1047@gmail.com>
2025-02-02 01:04:27 +0530

    auth started

---
+++ src/pages/api/auth/[...nextauth].js
@@ -0,0 +1,10 @@
+import NextAuth from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+
+export const authOptions = {
+    providers:[
+        CredentialsProvider({
+            name:"Credentials",
+        })
+    ]
+}
