78be94d3f4a446cf33d08ee4d6e2e6004f95b655
Vikrant Chauhan <vikrantchauhan1047@gmail.com>
2025-02-02 20:14:37 +0530

    added auth and google Oauth

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -1,10 +1,55 @@
 import NextAuth from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
+import Email from "next-auth/providers/email";
 
 export const authOptions = {
     providers:[
+        GoogleProvider({
+            clientId: process.env.GOOGLE_CLIENT_ID,
+            clientSecret : process.env.GOOGLE_CLIENT_SECRET,
+
+        }),
         CredentialsProvider({
             name:"Credentials",
+            credentials:{
+                email:{label:"Email",type:"email"},
+                password:{label:"Password",type:"password"},
+
+            },
+            async authorize(credentials){
+                // we make a dummy user here 
+                const user = {email:"user@example.com",password:"password123"};
+                if(credentials.email===user.email && credentials.password === user.password){
+                    return {id:1,email:user.email};
+                }
+                return null;
+            }
         })
-    ]
-}
+    ],
+    session :{
+        stratergy:"jwt",
+        maxAge:60*60*3, // session expires in 3 hours
+        /* updateAge:60*60 session will be refreshed every one hour to see if the user is active and increase the age plus one hour  */
+    },
+    pages:{
+        signIn:"/login",
+    },
+    callbacks:{
+        async jwt({token,user}){
+            if(user){
+                token.id = user.id;
+                token.email= user.email;
+
+            }
+            return token;
+
+        },
+        async session({session,token}){
+            session.id = token.id;
+            session.email= token.email;
+            return session;
+        },
+    },
+};
+
+export default NextAuth(authOptions);
